c68c644f4b0eead0748bfe933f092b3ed6f8f2f2
Lucas Trindade <lucasmedeirostrindade@gmail.com>
2025-09-09 17:38:25 -0300

    teste mais uma vez

--- src/indexing/chromaSetup.py
+++ src/indexing/chromaSetup.py
@@ -1,3 +1,4 @@
+import os
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_aws import BedrockEmbeddings
