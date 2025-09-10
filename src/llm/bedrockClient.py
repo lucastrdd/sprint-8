a58645b0c1344e199e0e0678f2cb63dcb9225429
Lucas Trindade <lucasmedeirostrindade@gmail.com>
2025-09-09 23:24:21 -0300

    nova-lite

--- src/llm/bedrockClient.py
+++ src/llm/bedrockClient.py
@@ -2,7 +2,7 @@ from langchain_aws import BedrockLLM
 
 def getLlm():
     return BedrockLLM(
-        model_id='amazon.nova-micro-v1:0',
+        model_id='amazon.nova-lite-v1:0',
         region_name='us-east-2',
         model_kwargs={'temperature': 0.3, 'maxTokenCount': 2048}
     )
